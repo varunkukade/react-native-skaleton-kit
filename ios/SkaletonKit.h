669ac232b4cb53e25e8d64aeb300f2fcf1acd29d
varunkukade <varunkukade999@gmail.com>
2025-01-01 14:12:12 +0530

    feat: backward compatibility with old arch for ios

--- ios/SkaletonKit.h
+++ ios/SkaletonKit.h
@@ -1,6 +1,12 @@
-
+#ifdef RCT_NEW_ARCH_ENABLED
 #import "generated/RNSkaletonKitSpec/RNSkaletonKitSpec.h"
 
 @interface SkaletonKit : NSObject <NativeSkaletonKitSpec>
+#else
+#import <React/RCTBridgeModule.h>
+
+@interface SkaletonKit: NSObject <RCTBridgeModule>
+
+#endif
 
 @end
--- ios/SkaletonKit.mm
+++ ios/SkaletonKit.mm
@@ -9,10 +9,13 @@ RCT_EXPORT_MODULE()
     return result;
 }
 
+// Don't compile this code when we build for the old architecture.
+#ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
     return std::make_shared<facebook::react::NativeSkaletonKitSpecJSI>(params);
 }
+#endif
 
 @end
